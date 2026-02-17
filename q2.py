724771dc41581cc845121c0f2b5b223845ec085f
nkatine2026 <nkatine2026@kehillahstudent.org>
2026-02-17 10:19:59 -0800

    f

--- q2.py
+++ q2.py
@@ -1,14 +1,13 @@
-# Make a random pet.
-
-# Choose:
-# Type of animal (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
-# Age (integer)
-age = None#REPLACE THIS WITH YOUR CODE
-# Color (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
-# Weight (float)
-weight = None#REPLACE THIS WITH YOUR CODE
-
-# Print a summary of your pet using an f-string
-print(f"Your pet is a {animal}")#REPLACE THIS WITH YOUR CODE
+import random
+animal = ["amphimbian", "mammal", "Reptile"]
+ani = random.choice(animal)
+age = ["4.20", "6.7", "6.9"]
+a = random.choice(age)
+color = ["orange", "black", "white"]
+col = random.choice(color)
+weight = ["Trump-sized", "normal-sized", "RFK Brain sized"]
+we = random.choice(weight)
+print(f"Your pet is a {ani}")
+print(f"Your pet is {a} years old")
+print(f"Your pet is {col}")
+print(f"Your pet is {we}")
--- q3.py
+++ q3.py
@@ -1,7 +1,12 @@
-suits = "♠♥♣♦"
-values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
-deck = []
+import random
+def ches():
+    suits = ["♠", "♥", "♣", "♦"]
+    c = random.choice(suits)
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    d = random.choice(values)
+    return c, d
+for i in range(5):
+    print(ches)
 # Make a deck of cards
 # `deck` should be a list of strings with a value and a suit, e.g. "4♣"
 
